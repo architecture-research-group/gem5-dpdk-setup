08085563eb534001a5d72cd925623accdaacd6bf
Johnson Umeike <c834u979@altra.ittc.ku.edu>
2024-05-18 02:09:57 -0500

    updating runscripts and guestscripts

--- run-scripts/run-droprate-exp.sh
+++ run-scripts/run-droprate-exp.sh
@@ -1,31 +1,16 @@
 #!/bin/bash 
 
-# for j in 1GHz 2GHz 4GHz;do
-  # for i in {9..12..1};do
-  #   ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i)) --packet-size 64 --freq 4GHz &
-  # done
-# done
-
-# wait
 
 # for j in 1 4 8 16;do
-#   for i in {5..11..1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i)) --packet-size 64 --freq 4GHz &
-#   done
+  for i in {1..15..1};do
+    ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i)) --packet-size 64 --freq 4GHz &
+  done
 # done
 
-# wait
 
-# for j in 1GHz 2GHz 4GHz;do
-#   for i in {21..25..1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i)) --packet-size 64 --freq 4GHz &
-#   done
-# done
-
-# wait
 
 # for j in 32 128 256 512;do
-  for i in {4..8..1};do
+  for i in {10..20..1};do
     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/2)) --packet-size 128 --freq 4GHz &
   done
 # done
@@ -33,152 +18,32 @@
 # wait
 
 # for j in 1GHz 2GHz 4GHz;do
-#   for i in {20..28..1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/2)) --packet-size 128 --freq 4GHz &
-#   done
-# done
-
-# wait
-
-# for j in 1GHz 2GHz 4GHz;do
-  for i in {6..10..1};do
+  for i in {20..35..1};do
     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/4)) --packet-size 256 --freq 4GHz &
   done
 # done
 
-# wait 
-
-# for j in 1GHz 2GHz 4GHz;do
-#   for i in {33..41..1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/4)) --packet-size 256 --freq 4GHz &
-#   done
-# done
-
-# wait 
-
-# for j in 32 128 256 512;do
-#   for i in {31..38..1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/4)) --packet-size 256 --freq 4GHz &
-#   done
-# done
-
 # wait
 
 # for j in 32 128 256 512;do
-  for i in {7..11..1};do
+  for i in {45..56..1};do
     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/8)) --packet-size 512 --freq 4GHz &
   done
 # done
 
-# wait
-
-# for j in 1GHz 2GHz 4GHz;do
-#   for i in {51..59..1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/8)) --packet-size 512 --freq 4GHz &
-#   done
-# done
-
-# wait
-
-# for j in 1GHz 2GHz 4GHz;do
-#   for i in {60..68.1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/8)) --packet-size 512 --freq 4GHz &
-#   done
-# done
-
-# wait
 
 # for j in 4GHz;do
-  for i in {8..12..1};do
+  for i in {50..57..1};do
     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/16)) --packet-size 1024 --freq 4GHz &
   done
 # done
 
-# wait
-
-# for j in 1GHz 2GHz 4GHz;do
-#   for i in {57..65..1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/16)) --packet-size 1024 --freq 4GHz &
-#   done
-# done
-
-# wait
-
-# # for j in 1GHz 2GHz 4GHz;do
-# #   for i in {66..71..1};do
-# #     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/16)) --packet-size 1024 --freq 4GHz &
-# #   done
-# # done
-
-# # wait
-# # for j in 1GHz;do
-# #   for i in {5..15..1};do
-# #     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/4)) --packet-size 256 --freq 4GHz &
-# #   done
-# # done
-
-# # wait 
-
-
-# # for j in 1GHz 2GHz;do
-# #   for i in {16..23..1};do
-# #     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/4)) --packet-size 256 --freq 4GHz &
-# #   done
-# # done
-
-# # wait 
-
-# # for j in 1GHz 2GHz;do
-# #   for i in 25 27 28 30;do
-# #     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/4)) --packet-size 256 --freq 4GHz &
-# #   done
-# # done
-
-# # wait 
-
-# # for j in 1GHz 2GHz;do
-# #   for i in 31 32 34 40 41 42 45 53 54 55 56;do
-# #     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((2150786*$i/4)) --packet-size 256 --freq 4GHz &
-# #   done
-# # done
-
 # # wait 
 
 # for j in 1 4 8 16;do
-  for i in {9..13..1};do
+  for i in {52..60..1};do
     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((88418*$i)) --packet-size 1518 --freq 4GHz &
   done
-# done
-
-# wait
-
-# for j in 1GHz 2GHz 4GHz;do
-#   for i in {54..62..1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((88418*$i)) --packet-size 1518 --freq 4GHz &
-#   done
-# done
-
-# wait
-
-# for j in 1GHz 2GHz 4GHz;do
-#   for i in {62..70..1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((88418*$i)) --packet-size 1518 --freq 4GHz &
-#   done
-# done
-
-# wait
-# for j in 1GHz 2GHz 4GHz;do
-#   for i in {77..87..1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((88418*$i)) --packet-size 1518 --freq 4GHz &
-#   done
-# done
-
-# wait
-
-# for j in 1GHz 2GHz 4GHz;do
-#   for i in {88..100..1};do
-#     ./l2fwd-ckp.sh --num-nics 1  --script dpdk-testpmd-touchfwd.sh --packet-rate $((88418*$i)) --packet-size 1518 --freq 4GHz &
-#   done
 # done
 
 wait
